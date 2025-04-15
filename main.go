5b5dd97c0dbd7b383181e899b975d0eb146d3ab0
root <root@ip-172-31-22-103.ec2.internal>
2025-04-15 15:24:40 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Format("Mon Jan 2 15:04:05 MST 2006")
+	fmt.Fprintf(w, "<h1>Current Date & Time:</h1><p>%s</p>", currentTime)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server at http://localhost:8080 ...")
+	http.ListenAndServe(":8080", nil)
+}
